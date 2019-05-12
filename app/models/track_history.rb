061154271744fa5eb0cf4d65be46ce3bbbe5adf1
Aden Aziz Hasnayakin <aden@kodelokus.com>
2019-05-12 21:32:53 +0700

    UserTrackHistory model

---
+++ app/models/track_history.rb
@@ -0,0 +1,3 @@
+class TrackHistory < ApplicationRecord
+  belongs_to :user
+end
---
+++ db/migrate/20190512143130_create_track_histories.rb
@@ -0,0 +1,12 @@
+class CreateTrackHistories < ActiveRecord::Migration[6.0]
+  def change
+    create_table :track_histories do |t|
+      t.string :noresi
+      t.string :expedition_type
+      t.string :status
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
