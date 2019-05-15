6649ff49334ee192ec48c4c16803ebde04aad032
Aden Aziz Hasnayakin <aden@kodelokus.com>
2019-05-15 13:31:21 +0700

    using credentials instead of secrets

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -4,12 +4,12 @@ class JsonWebToken
   # Encodes and signs JWT Payload with expiration
   def self.encode(payload)
     payload.reverse_merge!(meta)
-    JWT.encode(payload, Rails.application.secrets.secret_key_base)
+    JWT.encode(payload, Rails.application.credentials.secret_key_base)
   end
 
   # Decodes the JWT with the signed secret
   def self.decode(token)
-    JWT.decode(token, Rails.application.secrets.secret_key_base)
+    JWT.decode(token, Rails.application.credentials.secret_key_base)
   end
 
   # Validates the payload hash for expiration and meta claims
