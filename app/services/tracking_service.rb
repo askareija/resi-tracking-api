11050d61ddcad742a31698b0be301ee2e68bf622
Aden Aziz Hasnayakin <aden@kodelokus.com>
2019-05-23 17:01:26 +0700

    Using Hash instead of OpenStruct

--- app/services/tracking_service.rb
+++ app/services/tracking_service.rb
@@ -17,29 +17,26 @@ class TrackingService
     res = Net::HTTP.post_form(uri, 'resi' => @no_resi, 'cek' => '', 'jasa' => @expedition_type.delete("\n"))
     doc = Nokogiri::HTML(res.body)
 
-    # Sample Resi
-    # 020530015708819
-
-    info = OpenStruct.new
+    info = {}
 
     # Parsing general information
     table_info = doc.search('div .table-striped')
     if table_info.empty?
       @track_history.status = 'NOT FOUND'
       @track_history.save
-      
+
       return nil
     else
-      info.no_resi = table_info.first.search('tr')[0].search('td')[2].try(:text)
-      info.status = table_info.first.search('tr')[1].search('td')[2].try(:text)
-      info.expedition_type = table_info.first.search('tr')[2].search('td')[2].try(:text)
-      info.date = table_info.first.search('tr')[3].search('td')[2].try(:text)
-      info.sender = table_info.first.search('tr')[4].search('td')[2].try(:text)
-      info.origin = table_info.first.search('tr')[5].search('td')[2].try(:text)
-      info.recipient = table_info.first.search('tr')[6].search('td')[2].try(:text)
-      info.recipient_address = table_info.first.search('tr')[7].search('td')[2].try(:text)
+      info[:no_resi] = table_info.first.search('tr')[0].search('td')[2].try(:text)
+      info[:status] = table_info.first.search('tr')[1].search('td')[2].try(:text)
+      info[:expedition_type] = table_info.first.search('tr')[2].search('td')[2].try(:text)
+      info[:date] = table_info.first.search('tr')[3].search('td')[2].try(:text)
+      info[:sender] = table_info.first.search('tr')[4].search('td')[2].try(:text)
+      info[:origin] = table_info.first.search('tr')[5].search('td')[2].try(:text)
+      info[:recipient] = table_info.first.search('tr')[6].search('td')[2].try(:text)
+      info[:recipient_address] = table_info.first.search('tr')[7].search('td')[2].try(:text)
 
-      info.details = ({})
+      info[:details] = ({})
       doc.search('table tbody').first.search('tr').each_with_index do |row, i|
         detail = {}
         detail[:date] = Time.zone.parse(row.search('td')[0].try(:text))
